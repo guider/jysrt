563d28a626e92bfead11c776b07fe391ca0faab7
张金伟 <guider@yeah.net>
2021-04-09 11:38:07 +0800

    add git ignore

--- node_modules/subtitle/src/stringify.ts
+++
@@ -1,7 +0,0 @@
-import { map, FormatOptions, Node } from '.'
-import { Formatter } from './Formatter'
-
-export const stringify = (options: FormatOptions) => {
-  const formatter = new Formatter(options)
-  return map((chunk: Node) => formatter.format(chunk))
-}
